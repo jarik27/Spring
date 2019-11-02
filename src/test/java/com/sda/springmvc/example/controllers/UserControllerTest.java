da34b63270a9d930512b4f2521294e092ab2f34f
Dmitry Zinkevich <dmitry.zinkevich@proekspert.ee>
2019-11-02 14:20:37 +0200

    Add test for new user registration

--- src/test/java/com/sda/springmvc/example/controllers/UserControllerTest.java
+++ src/test/java/com/sda/springmvc/example/controllers/UserControllerTest.java
@@ -1,20 +1,22 @@
 package com.sda.springmvc.example.controllers;
 
+import com.sda.springmvc.example.entities.User;
 import com.sda.springmvc.example.repositories.UserRepository;
 import com.sda.springmvc.example.validation.AgeValidationService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
@@ -31,8 +33,6 @@ public class UserControllerTest {
     @MockBean
     private AgeValidationService ageValidationService;
 
-    // Ctrl K
-
     @Test
     public void should_return_main_page() throws Exception {
         mockMvc
@@ -42,5 +42,28 @@ public class UserControllerTest {
                 .andExpect(status().isOk());
 
         verify(userRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void should_return_registration_form() throws Exception {
+        mockMvc
+                .perform(get("/signup"))
+                .andExpect(model().attributeExists("user"))
+                .andExpect(view().name("user-add"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void should_not_register_user_with_invalid_age() throws Exception {
+        final MockHttpServletRequestBuilder post
+                = post("/adduser").param("dateOfBirth", "2011-12-03");
+
+        mockMvc
+                .perform(post)
+                .andExpect(view().name("user-age-not-valid"))
+                .andExpect(status().isOk());
+
+        verify(ageValidationService, times(1))
+                .isValid(any(User.class));
     }
 }
